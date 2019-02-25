47f8ec09f25da3333a2729fca188ec7521d0ece4
Petrov Vladimir <neoman36@gmail.com>
2019-02-25 16:30:23 +0300

    Add method List.append

--- linkedlist.py
+++ linkedlist.py
@@ -4,10 +4,23 @@ class List:
         self.next = next_
 
     def __iter__(self):
+        for l in self._iter():
+            yield l.value
+
+    def _iter(self):
         current = self
         while current:
-            yield current.value
+            yield current
             current = current.next
 
     def print(self, sep=' '):
         print(sep.join(str(v) for v in self))
+
+    def get_head(self):
+        head = self
+        for head in self._iter():
+            pass
+        return head
+
+    def append(self, value):
+        self.get_head().next = List(value)
