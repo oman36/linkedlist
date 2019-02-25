44655d5e924596cfeade9c1a01de3873403f1c25
Petrov Vladimir <neoman36@gmail.com>
2019-02-25 15:50:19 +0300

    Add test for List.print

--- testlinkedlist.py
+++ testlinkedlist.py
@@ -1,6 +1,20 @@
+import sys
 import unittest
+from contextlib import contextmanager
+from io import StringIO
 
 from linkedlist import List
+
+
+@contextmanager
+def captured_output():
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        yield sys.stdout, sys.stderr
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
 
 
 class TestListMethods(unittest.TestCase):
@@ -12,6 +26,16 @@ class TestListMethods(unittest.TestCase):
             vals.append(v)
 
         self.assertEqual(vals, original)
+
+    def test_print(self):
+        list_ = List(1, List(2, List(3)))
+        with captured_output() as (out, err):
+            list_.print()
+            self.assertEqual(out.getvalue().strip(), '1 2 3')
+
+        with captured_output() as (out, err):
+            list_.print('\t')
+            self.assertEqual(out.getvalue().strip(), '1\t2\t3')
 
 
 if __name__ == '__main__':
